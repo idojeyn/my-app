9cc706bf04c9034625155f5f232b8e91bb159028
idojeyn <idojeyn0405@gmail.com>
2025-06-19 10:08:00 +0500

    button

---
+++ src/menu/menu.js
@@ -0,0 +1,17 @@
+import './menu.css'
+
+
+
+const Menu = () => {
+    return (
+        <div className='width border'>
+            <h2>Menu</h2>
+            <a href="a">Today</a>
+            <a href="a">yesterday</a>
+            <a href="a"> Last Week</a>
+            <a href="a">Archives</a>
+        </div>
+    )
+}
+
+export default Menu;
